f10af5a0eebc138c8ef3be0a789e3df576711228
sungw0o <sungwoo515@gmail.com>
2025-03-22 18:35:03 +0900

    feat : 프로젝트 초기 설정

---
+++ src/main/java/hello/booktown/BookTownApplication.java
@@ -0,0 +1,13 @@
+package hello.booktown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookTownApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hello/booktown/BookTownApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.booktown;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookTownApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
