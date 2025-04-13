8bdd28aeaf47918098275976fa499039d3932863
sungw0o <sungwoo515@gmail.com>
2025-04-13 18:56:42 +0900

    [#29] : fix : 접근 경로 수정

--- src/main/java/hello/booktown/controller/BookController.java
+++ src/main/java/hello/booktown/controller/BookController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/book")
+@RequestMapping("api/book")
 public class BookController {
 
     private final BookService bookService;
