1ee2e398568c2f82741aca35dfd50c0d85e19624
sungw0o <sungwoo515@gmail.com>
2025-04-20 20:48:07 +0900

    [#29] : fix : JSON 응답 에러 수정

--- src/main/java/hello/booktown/domain/BookSummary.java
+++ src/main/java/hello/booktown/domain/BookSummary.java
@@ -1,5 +1,6 @@
 package hello.booktown.domain;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -21,5 +22,6 @@ public class BookSummary {
     private Book book;
 
     @OneToMany(mappedBy = "bookSummary", cascade = CascadeType.ALL)
+    @JsonManagedReference
     private List<SummaryScene> scenes = new ArrayList<>();
 }
--- src/main/java/hello/booktown/domain/SummaryScene.java
+++ src/main/java/hello/booktown/domain/SummaryScene.java
@@ -1,5 +1,6 @@
 package hello.booktown.domain;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -11,6 +12,7 @@ public class SummaryScene {
     private Long id;
 
     @ManyToOne
+    @JsonBackReference
     private BookSummary bookSummary;
 
     private int pageNumber;
